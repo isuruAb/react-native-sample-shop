8bc7ce9fc6085ebe8e0012625f21346021ec1a10
isuruAb <vpisabeywardana@gmail.com>
2018-10-24 17:31:50 +0800

    Fixes: #16 items can be removed from cart

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -87,8 +87,6 @@ class Dashboard extends Component {
                                     </View>
                                 </TouchableOpacity>
                             </View>
-
-
                         )}
                     />
                 </View>
