9f88b1557bac374cb31649b4a1c43c22705cf428
isuruAb <vpisabeywardana@gmail.com>
2018-10-26 10:10:44 +0800

    Add checkout button and screen

--- Navigator.js
+++ Navigator.js
@@ -3,13 +3,14 @@ import Login from './components/Login';
 import Dashboard from './components/Dashboard';
 import Single from './components/Single';
 import Cart from './components/Cart';
+import Checkout from './components/Checkout';
 
 const Navigation = createStackNavigator({
     LoginScreen: { screen: Login },
     DashboardScreen: { screen: Dashboard },
     SingleScreen: { screen: Single },
-    CartScreen: { screen: Cart }
-
+    CartScreen: { screen: Cart },
+    CheckoutScreen: { screen: Checkout }
 });
 
 export default Navigation;
